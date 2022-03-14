4a9b7ab863a0570085ee1fcf9a8c6a5c328e6bcd
Kalghor <73907415+Kalghor@users.noreply.github.com>
2022-03-14 17:11:27 +0100

    Add files via upload

---
+++ src.cpp
@@ -0,0 +1,31 @@
+#include <glad/glad.h>
+#include <glfw3.h>
+
+constexpr int WIN_SZ_X = 1550, WIN_SZ_Y = 850;
+
+int main() {
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 5);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* window = glfwCreateWindow(WIN_SZ_X, WIN_SZ_Y, "Kulki Newtona", nullptr, nullptr);
+	if (!window)
+		return 1;
+
+	glfwMakeContextCurrent(window);
+	gladLoadGL();
+
+	while (!glfwWindowShouldClose(window)) {
+		if (glfwGetKey(window, GLFW_KEY_Q) == GLFW_PRESS) {
+			glfwSetWindowShouldClose(window, 1);
+		}
+		glClearColor(0.2f, 0.3f, 0.7f, 1.f);
+		glClear(GL_COLOR_BUFFER_BIT);
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+	return 1;
+}
